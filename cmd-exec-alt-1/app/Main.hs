3fb19355d25b1282a7a4361ed3f144098a67c27b
Tibor Fasanga <tibor.fasanga@nokia.com>
2024-03-04 14:05:05 -0500

    added simple executable

--- cmd-exec-alt-1/app/Main.hs
+++ cmd-exec-alt-1/app/Main.hs
@@ -1,7 +1,9 @@
 module Main (main) where
 
+
 import Common.Err
 import Common.Result
+import Machines
 
 main :: IO ()
 main = run >>= handleResult
@@ -11,7 +13,14 @@ run :: IO (Result ())
 run = try runProgram
 
 runProgram :: IO ()
-runProgram = undefined
+runProgram = do
+  let ms = getMachines
+  let m1 = getMachine ms DevelopmentMachine
+  let m2 = getMachine ms BuildMachine
+  let m3 = getMachine ms TestMachine
+  putStrLn (show m1)
+  putStrLn (show m2)
+  putStrLn (show m3)
 
 handleResult :: Result () -> IO ()
 handleResult (Left err) = putStrLn (show err)
